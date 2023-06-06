b26ada18102ad3de12493623ffa81895b544da0a
Mengwa Ugo <mengwa44@yahoo.com>
2023-06-06 02:51:26 +0100

    added pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,37 @@
 import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+import Homepage from "./Pages/home/Homepage"
 import "./App.css";
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import "./index.css";
 
 function App() {
+  const Layout = () => {
+    return (
+      <>
+        <Navbar />
+        <Outlet />
+        <Footer />
+      </>
+    );
+  };
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children : [
+        {
+          path : "/",
+          element : <Homepage />
+        }
+      ]
+    },
+  ]);
   return (
     <div>
-      <Navbar />
+      <RouterProvider router={router} />
     </div>
   );
 }
---
+++ frontend/src/Pages/message/Messge.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Message.scss"
+
+const Messge = () => {
+  return (
+    <div>Messge</div>
+  )
+}
+
+export default Messge
